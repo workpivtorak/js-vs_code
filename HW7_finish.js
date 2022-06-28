1617a99a29ec1f5e74242d59d7bb0976175d647c
Yurii Pivtorak <106430080+workpivtorak@users.noreply.github.com>
2022-06-28 21:10:54 +0300

    Update HW7_finish.js

--- HW7_finish.js
+++ HW7_finish.js
@@ -10,17 +10,17 @@ console.log("item_3 =", item_3)
 let item_4
 item_4 = "Yolochka"
 console.log(item_4)
-console.log(item_3 + " " + item_4)
-console.log(item_3 + " * " + item_4 )
+console.log(item_3 + item_4)
+console.log(item_3 * item_4 )
 let item_5
 item_5 = item_3
 let item_6 = 15
 let item_6_type = typeof(item_6)
 console.log("item_6 ==", typeof(item_6)) 
 console.log("item_6_type ==", item_6_type)
-let item_7 = item_6 = "Fanny_home_work"
-console.log("item_7 ==", typeof(item_7))
-let item_7_type = typeof(item_7)
+item_7 = String(item_6)
+let item_7_type
+item_7_type = typeof(item_7)
 console.log("item_7 ==", typeof(item_7))  
 console.log("item_7_type ==", item_7_type)
 let age_1 = 10
@@ -28,13 +28,10 @@ let age_2 = 18
 let age_3 = 60
 if (age_1 < age_2) {
 console.log ("You don't have access cause your age is " + age_1 + " " + "It's less then")
-}
-else if (age_1 >= age_2 , age_1 < age_3) {
+} else if (age_1 >= age_2 && age_1 < age_3) {
 console.log("Welcome !")
-}
-else if (age_1 > age_3) {
+} else if (age_1 > age_3) {
 console.log("Keep calm and look Culture channel")
-}
-else {
-    console.log("Tehnical work")    
+} else {
+console.log("Tehnical work")    
 }
